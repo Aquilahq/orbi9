4ae6ba8721682bf90274afd13ff0d8aa4f20f8cf
Aquila <aquila@AQUILA.localdomain>
2026-08-28 10:33:06 -0700

    refresh admin product image mapping

--- product-editor.js
+++ product-editor.js
@@ -1,4 +1,4 @@
-import { supabase, fetchProducts, saveProduct, removeProduct, productFromRow } from './orbi9-database.js';
+import { supabase, fetchProducts, saveProduct, removeProduct, productFromRow } from './orbi9-database.js?v=products-images-2';
 
 const $ = id => document.getElementById(id);
 let products = [], current = null, mediaIndex = 0, pendingFiles = [];
