55ac3ec5f591e430de4421b673efc97d0072be27
Aquila <aquila@AQUILA.localdomain>
2026-08-28 11:16:47 -0700

    remove WordPress deployment path

--- Orbi9/woocommerce.php
+++
@@ -1,8 +0,0 @@
-<?php
-get_header();
-if (function_exists('woocommerce_content')) {
-    woocommerce_content();
-} else {
-    echo '<main class="frame"><p>WooCommerce is required for the shop.</p></main>';
-}
-get_footer();
