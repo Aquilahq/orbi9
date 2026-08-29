87bce96c8f98b317e01778370ee9d4091d76ac03
Aquila <aquila@AQUILA.localdomain>
2026-08-28 20:36:09 -0700

    use Stripe automatic payment methods

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post('/api/create-checkout-session', async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
       mode: 'payment',
-      payment_method_types: ['card', 'paypal'],
+      automatic_payment_methods: { enabled: true },
       line_items: lineItems,
       success_url: `${siteUrl}/?checkout=success&session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${siteUrl}/?checkout=cancelled`,
